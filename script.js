8885d752d83f5667f55e64a0f4b28efc0999acba
GabrielDuarte14 <gabriel.duarte@sptech.school>
2022-05-06 14:38:26 -0300

    atualizacao de msg

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function calcular(){
         var total = 18 - notaac1 - notaac2;
      
         if(total>0 && total<=10) resposta.innerHTML = `Para que você passe de semestre, a nota da AC3 deve ser maior ou igual a ${total}`
-        else if(total<0) resposta.innerHTML = `Você já alcançou a média das AC's, agora presta atenção na Sprint!!`
+        else if(total<0) resposta.innerHTML = `Você já alcançou a média das AC's, sem preocupação com a AC3, agora presta atenção na Sprint!!`
         else resposta.innerHTML = `Infelizmente você precisa tirar mais que 10 para alcançar a média das ACs`
  
     }
